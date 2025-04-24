64a812f6b835a3b583948d477428e86c29bb741c
Matheus Glauber <glauber.jordao1995@gmail.com>
2025-04-23 21:39:10 -0300

    [#6] - Configuração do `Prettier`.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tabnews-study
+
+Implementação do [tabnews](https://www.tabnews.com.br/)
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home() {
-    var nome = 'Matheus Glauber'
-    var namorada = 'Laissa Albuquerque'
-    return <h1>Olá {namorada}, {nome} te ama pra CARAMBA!</h1>
+  var nome = "Matheus Glauber";
+  var namorada = "Laissa Albuquerque";
+  return (
+    <h1>
+      Olá {namorada}, {nome} te ama pra CARAMBA!
+    </h1>
+  );
 }
-  
+
 export default Home;
