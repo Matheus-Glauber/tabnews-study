396807d9a17a681a835d5c03da8d94339c91393a
Matheus Glauber <glauber.jordao1995@gmail.com>
2025-05-27 21:10:02 -0300

    [#21] - Removido model e teste para calculadora.

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(parcela1, parcela2) {
-  if (typeof parcela1 !== "number" || typeof parcela2 !== "number") {
-    return "Erro";
-  }
-  return parcela1 + parcela2;
-}
-
-exports.somar = somar;
